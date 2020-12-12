242d4075a09d7f4acefafc51afd5ad324ddac07d
Gergely Pék <gergely.pek@stud.uni-corvinus.hu>
2020-12-12 14:09:28 +0100

    commit

--- IRF_YMOOIX/IRF_YMOOIX/Kimutatas.cs
+++ IRF_YMOOIX/IRF_YMOOIX/Kimutatas.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using IRF_YMOOIX.Osztalyok;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,36 @@ namespace IRF_YMOOIX
 {
     public partial class Kimutatas : Form
     {
+        List<Adatok> adat = new List<Adatok>();
+        List<Orszagok> orsz = new List<Orszagok>();
+
+        
         public Kimutatas()
         {
             InitializeComponent();
+
+            LoadData();
+            //LoadNames();
+        }
+
+        private void LoadData()
+        {
+            adat.Clear();
+
+            using (StreamReader sr = new StreamReader("adatok.csv", Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(',');
+                    Adatok a = new Adatok();
+                    a.orszag = line[0];
+                    a.ev = int.Parse(line[1]);
+                    a.nepesseg = long.Parse(line[2]);
+
+                    adat.Add(a);
+                }
+
+            }
         }
     }
 }
