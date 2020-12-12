9306a2ddfc30cae38a4bac9234675367fc4e23bb
Gergely Pék <gergely.pek@stud.uni-corvinus.hu>
2020-12-12 13:18:42 +0100

    commit

--- IRF_YMOOIX/IRF_YMOOIX/Kezdolap.cs
+++ IRF_YMOOIX/IRF_YMOOIX/Kezdolap.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace IRF_YMOOIX
 {
-    public partial class Form1 : Form
+    public partial class Kezdolap : Form
     {
-        public Form1()
+        public Kezdolap()
         {
             InitializeComponent();
         }
--- IRF_YMOOIX/IRF_YMOOIX/Kimutatas.Designer.cs
+++ IRF_YMOOIX/IRF_YMOOIX/Kimutatas.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Kimutatas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Kimutatas";
+            this.Name = "Kimutatas";
+            this.Text = "Kimutatás";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
