42d6edad93e383f9f4b1f86e7733ecf8e2367ccb
Gergely Pék <gergely.pek@stud.uni-corvinus.hu>
2020-12-12 13:58:34 +0100

    commit

---
+++ IRF_YMOOIX/IRF_YMOOIX/Osztalyok/Orszagok.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_YMOOIX.Osztalyok
+{
+    class Orszagok
+    {
+        public string nev { get; set; }
+    }
+}
