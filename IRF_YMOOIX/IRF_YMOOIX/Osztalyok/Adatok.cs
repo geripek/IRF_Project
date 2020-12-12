86ff98a80b321a17401b25616ed955614a9984dc
Gergely Pék <gergely.pek@stud.uni-corvinus.hu>
2020-12-12 14:00:08 +0100

    commit

---
+++ IRF_YMOOIX/IRF_YMOOIX/Osztalyok/Adatok.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_YMOOIX.Osztalyok
+{
+    class Adatok
+    {
+        public string orszag { get; set; }
+        public int ev { get; set; }
+        public long nepesseg { get; set; }
+    }
+}
