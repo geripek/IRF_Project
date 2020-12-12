6484d33abb690d7ca83457465a2facadc027af91
Gergely Pék <gergely.pek@stud.uni-corvinus.hu>
2020-12-12 13:56:59 +0100

    commit

---
+++ IRF_YMOOIX/IRF_YMOOIX/Osztalyok/Szovegek.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IRF_YMOOIX.Osztalyok
+{
+    class Szovegek: Label
+    {
+       public Szovegek()
+        {
+            //this.Font = new Font("Times New Roman", 16);
+        }
+    }
+}
---
+++ IRF_YMOOIX/IRF_YMOOIX/Osztalyok/Timer1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace IRF_YMOOIX.Osztalyok
+{
+    class Timer1: Timer
+    {
+        public Timer1()
+        {
+            this.Interval = 5000;
+        }
+    }
+}
